de9eca23ac839e50178be3353c13b9f27fd09bd6
sriby7 <sri_by@yahoo.com>
2017-11-28 11:40:53 +0530

    HelloWorld Changes.

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -28,6 +28,8 @@ bool HelloWorld::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+        //Srinivas
+    
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
