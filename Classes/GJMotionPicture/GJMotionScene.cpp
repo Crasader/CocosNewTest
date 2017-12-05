27016051b117495396e152c56bd409164bec6254
SivabalanWinsols <ksivabalan.winsols@gmail.com>
2017-12-05 21:47:12 +0530

    Label Added in Motion Picture

--- Classes/GJMotionPicture/GJMotionScene.cpp
+++ Classes/GJMotionPicture/GJMotionScene.cpp
@@ -11,6 +11,7 @@ USING_NS_CC;
 
 Scene* MotionScene::createScene()
 {
+    
     return MotionScene::create();
 }
 
@@ -34,6 +35,8 @@ bool MotionScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
+   // label = Label::createWithSystemFont("hello","Arial.ttf",40);
+    
         auto motionSprite1 = Sprite::create("MP1.png");
     
         if (motionSprite1 == nullptr)
@@ -48,43 +51,49 @@ bool MotionScene::init()
         }
     
         auto MPfade = FadeIn::create(1.0f);
-        auto firstMPmove = MoveBy::create(5, Vec2(-50,0));
-        auto MPfade_reverse = MPfade->reverse();
-    
-        auto MPdelay = DelayTime::create(3);
+        auto firstMPscale = ScaleTo::create(15, 1.2f);
+        auto firstMPmove = MoveBy::create(60, Vec2(50,-50));
+        auto firstMPmove1 = MoveBy::create(10, Vec2(-10,10));
+        //auto MPfade_reverse = MPfade->reverse();
     
         motionSprite1->setOpacity(0);
-        motionSprite1->runAction( firstMPmove);
-        motionSprite1->runAction( Sequence::create( MPfade,MPdelay,MPfade_reverse, nullptr));
+        motionSprite1->runAction( firstMPscale);
+        motionSprite1->runAction( Sequence::create( MPfade,firstMPmove,firstMPmove1, nullptr));
     
+        //this->scheduleOnce(CC_SCHEDULE_SELECTOR(MotionScene::afterFirstMotion), 10.0f);
+    
+    
+    
+        charSprite1 = Sprite::create("Character1.png");
+        charSprite1->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+        charSprite1->setScale(0);
+        this->addChild(charSprite1, 1);
+    
+        charSprite2 = Sprite::create("Character2.png");
+        charSprite2->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+        charSprite2->setScale(0);
+        this->addChild(charSprite2, 1);
+ 
 
-    //this->scheduleOnce(CC_SCHEDULE_SELECTOR(MotionScene::afterFirstMotion), 10.0f);
-    
-    
+    this->MPLayer1 =Layer::create();
+    this->addChild(MPLayer1,1);
+    MPLayer1->setScale(0);
     auto motionSprite2 = Sprite::create("MP2.png");
     
     if (motionSprite2 == nullptr)
     {
-        problemLoading("'MP1.png'");
+        problemLoading("'MP2.png'");
     }
     else
     {
         motionSprite2->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
         motionSprite2->setScale(1.1);
-        this->addChild(motionSprite2, 0);
+        MPLayer1->addChild(motionSprite2, 0);
     }
     
-    auto secondMPscale = ScaleTo::create(5, 1.2f);
-    auto secondMPmove = MoveBy::create(5, Vec2(0,-30));
-
-    
-    auto secondMPdelay = DelayTime::create(5);
-    
-    motionSprite2->setOpacity(0);
-    motionSprite2->runAction( Sequence::create( secondMPdelay,secondMPscale, nullptr));
-    motionSprite2->runAction( Sequence::create( secondMPdelay,secondMPmove, nullptr));
-    motionSprite2->runAction( Sequence::create( secondMPdelay,MPfade,MPdelay,MPfade_reverse, nullptr));
-    
+    this->MPLayer2 =Layer::create();
+    this->addChild(MPLayer2,2);
+    MPLayer2->setScale(0);
     auto motionSprite3 = Sprite::create("MP3.png");
     
     if (motionSprite3 == nullptr)
@@ -95,15 +104,18 @@ bool MotionScene::init()
     {
         motionSprite3->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
         motionSprite3->setScale(1.1);
-        this->addChild(motionSprite3, 0);
+        MPLayer2->addChild(motionSprite3, 0);
     }
+
+    auto touchListener = EventListenerTouchOneByOne::create();
     
-    auto thirdMPmove = MoveBy::create(5, Vec2(50,0));
-    auto thirdMPdelay = DelayTime::create(10);
+    touchListener->onTouchBegan = CC_CALLBACK_2(MotionScene::onTouchBegan, this);
+    touchListener->onTouchEnded = CC_CALLBACK_2(MotionScene::onTouchEnded, this);
+    touchListener->onTouchMoved = CC_CALLBACK_2(MotionScene::onTouchMoved, this);
+    touchListener->onTouchCancelled = CC_CALLBACK_2(MotionScene::onTouchCancelled, this);
     
-    motionSprite3->setOpacity(0);
-    motionSprite3->runAction( Sequence::create( thirdMPdelay,thirdMPmove, nullptr));
-    motionSprite3->runAction( Sequence::create( thirdMPdelay,MPfade,MPdelay,MPfade_reverse, nullptr));
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
+
    
     CCLOG("Entered ");
     return true;
@@ -115,5 +127,228 @@ void MotionScene::functionfun1()
 
 void MotionScene::afterFirstMotion(float dt)
 {
+    CCLOG("comee");
+}
+
+bool MotionScene::onTouchBegan(Touch* touch, Event* event)
+{
+    cocos2d::log("touch begin");
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
+    
+    //label = LabelTTF::create("", "Arial", 40);
+    label = Label::createWithSystemFont("hello","Arial.ttf",40);
+    //label=Label::createWithTTF("", "fonts/Marker Felt.ttf", 50);
+    Label_val++;
+    if(Label_val==1)
+    {
+        
+        label->setString("Label1");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+        
+    }
+    else if(Label_val==2)
+    {
+        label->setString("Label2");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+        
+        
+    }
+    else if(Label_val==3)
+    {
+        label->setString("Label3");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+        
+    }
+    else if(Label_val==4)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("Label4");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+        
+    }
+    else if(Label_val==5)
+    {
+        charSprite1->setScale(0);
+        charSprite2->setScale(1);
+        label->setString("");
+        label->setString("Label5");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==6)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label6");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==7)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label7");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==8)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label8");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==9)
+    {
+        charSprite1->setScale(0);
+        charSprite2->setScale(1);
+        label->setString("");
+        label->setString("Label9");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==10)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label10");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==11)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label11");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==12)
+    {
+        charSprite1->setScale(1);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label12");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==13)
+    {
+        charSprite1->setScale(0);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label13");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+        MPLayer1->setOpacity(0);
+        
+        auto MPfade = FadeIn::create(1.0f);
+        auto firstMPmove = MoveBy::create(15, Vec2(-40,0));
+   
+        MPLayer1->runAction( Sequence::create( MPfade,firstMPmove, nullptr));
+
+         MPLayer1->setScale(1);
+    }
+    else if(Label_val==14)
+    {
+        label->setString("");
+        label->setString("Label14");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==15)
+    {
+        label->setString("");
+        label->setString("Label15");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==16)
+    {
+        label->setString("");
+        label->setString("Label16");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==17)
+    {
+        label->setString("");
+        label->setString("Label17");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==18)
+    {
+        charSprite1->setScale(0);
+        charSprite2->setScale(0);
+        label->setString("");
+        label->setString("Label13");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+        MPLayer2->setOpacity(0);
+        
+        auto MPfade = FadeIn::create(1.0f);
+        auto firstMPmove = MoveBy::create(30, Vec2(-10,0));
+        MPLayer2->runAction( Sequence::create( MPfade,firstMPmove, nullptr));
+        
+        MPLayer2->setScale(1);
+    }
+    else if(Label_val==19)
+    {
+        label->setString("");
+        label->setString("Label19");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==20)
+    {
+        label->setString("");
+        label->setString("Label20");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==21)
+    {
+        label->setString("");
+        label->setString("Label21");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    else if(Label_val==22)
+    {
+        label->setString("");
+        label->setString("Label22");
+        label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height/4));
+    }
+    this->addChild(label, 3);
+    return true;
+}
+
+void MotionScene::onTouchEnded(Touch* touch, Event* event)
+{
+    cocos2d::log("touch ended");
+}
+
+void MotionScene::onTouchMoved(Touch* touch, Event* event)
+{
+    cocos2d::log("touch moved");
+}
+
+void MotionScene::onTouchCancelled(Touch* touch, Event* event)
+{
+    cocos2d::log("touch cancelled");
 }
